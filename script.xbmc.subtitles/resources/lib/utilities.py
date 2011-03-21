36d279695eb187aa01a6c81330c015adeec0a7a1
amet <amet.nospam@gmail.com>
2011-03-21 18:30:47 +0400

    add languageTranslate() function

--- script.xbmc.subtitles/resources/lib/utilities.py
+++ script.xbmc.subtitles/resources/lib/utilities.py
@@ -8,10 +8,114 @@ import struct
 
 __scriptname__ = sys.modules[ "__main__" ].__scriptname__
 
+###-------------------------  Languages  ------------------###############
+
+LANGUAGES = (
+    
+    # Full Language name[0]     podnapisi[1]  ISO 639-1[2]   ISO 639-1 Code[3]   Script Setting Language[4]
+    
+    ("Albanian"                   , "29",       "sq",            "alb",                 "0"  ),
+    ("Arabic"                     , "12",       "ar",            "ara",                 "1"  ),
+    ("Belarusian"                 , "0" ,       "hy",            "arm",                 "2"  ),
+    ("Bosnian"                    , "10",       "bs",            "bos",                 "3"  ),
+    ("Bulgarian"                  , "33",       "bg",            "bul",                 "4"  ),
+    ("Catalan"                    , "53",       "ca",            "cat",                 "5"  ),
+    ("Chinese"                    , "17",       "zh",            "chi",                 "6"  ),
+    ("Croatian"                   , "38",       "hr",            "hrv",                 "7"  ),
+    ("Czech"                      , "7",        "cs",            "cze",                 "8"  ),
+    ("Danish"                     , "24",       "da",            "dan",                 "9"  ),
+    ("Dutch"                      , "23",       "nl",            "dut",                 "10" ),
+    ("English"                    , "2",        "en",            "eng",                 "11" ),
+    ("Estonian"                   , "20",       "et",            "est",                 "12" ),
+    ("Farsi"                      , "52",       "fa",            "per",                 "13" ),
+    ("Persian"                    , "52",       "fa",            "per",                 "13" ),
+    ("Finnish"                    , "31",       "fi",            "fin",                 "14" ),
+    ("French"                     , "8",        "fr",            "fre",                 "15" ),
+    ("German"                     , "5",        "de",            "ger",                 "16" ),
+    ("Greek"                      , "16",       "el",            "ell",                 "17" ),
+    ("Hebrew"                     , "22",       "he",            "heb",                 "18" ),
+    ("Hindi"                      , "42",       "hi",            "hin",                 "19" ),
+    ("Hungarian"                  , "15",       "hu",            "hun",                 "20" ),
+    ("Icelandic"                  , "6",        "is",            "ice",                 "21" ),
+    ("Indonesian"                 , "0",        "id",            "ind",                 "22" ),
+    ("Italian"                    , "9",        "it",            "ita",                 "23" ),
+    ("Japanese"                   , "11",       "ja",            "jpn",                 "24" ),
+    ("Korean"                     , "4",        "ko",            "kor",                 "25" ),
+    ("Latvian"                    , "21",       "lv",            "lav",                 "26" ),
+    ("Lithuanian"                 , "0",        "lt",            "lit",                 "27" ),
+    ("Macedonian"                 , "35",       "mk",            "mac",                 "28" ),
+    ("Norwegian"                  , "3",        "no",            "nor",                 "29" ),
+    ("Polish"                     , "26",       "pl",            "pol",                 "30" ),
+    ("Portuguese"                 , "32",       "pt",            "por",                 "31" ),
+    ("PortugueseBrazil"           , "0",        "pb",            "pob",                 "32" ),
+    ("Portuguese (Brazil)"        , "0",        "pb",            "pob",                 "32" ),
+    ("Brazilian"                  , "0",        "pb",            "pob",                 "32" ),
+    ("Romanian"                   , "13",       "ro",            "rum",                 "33" ),
+    ("Russian"                    , "27",       "ru",            "rus",                 "34" ),
+    ("Serbian"                    , "36",       "sr",            "scc",                 "35" ),
+    ("Slovak"                     , "37",       "sk",            "slo",                 "36" ),
+    ("Slovenian"                  , "1",        "sl",            "slv",                 "37" ),
+    ("Spanish"                    , "28",       "es",            "spa",                 "38" ),
+    ("Swedish"                    , "25",       "sv",            "swe",                 "39" ),
+    ("Thai"                       , "0",        "th",            "tha",                 "40" ),
+    ("Turkish"                    , "30",       "tr",            "tur",                 "41" ),
+    ("Ukrainian"                  , "46",       "uk",            "ukr",                 "42" ),
+    ("Vietnamese"                 , "51",       "vi",            "vie",                 "43" ),
+    ("BosnianLatin"               , "10",       "bs",            "bos",                 "100"),
+    ("English (US)"               , "2",        "en",            "eng",                 "100"),
+    ("English (UK)"               , "2",        "en",            "eng",                 "100"),
+    ("Portuguese (Brazilian)"     , "0",        "pt-br",         "pob",                 "100"),
+    ("Español (Latinoamérica)"  , "28",       "es",            "spa",                 "100"),
+    ("Español (España)"         , "28",       "es",            "spa",                 "100"),
+    ("Spanish (Latin America)"    , "28",       "es",            "spa",                 "100"),
+    ("Español"                   , "28",       "es",            "spa",                 "100"),
+    ("SerbianLatin"               , "36",       "sr",            "scc",                 "100"),
+    ("Spanish (Spain)"            , "28",       "es",            "spa",                 "100"),
+    ("Chinese (Traditional)"      , "17",       "zh",            "chi",                 "100"),
+    ("Chinese (Simplified)"       , "17",       "zh",            "chi",                 "100") )
+
+
 ###-------------------------  Log  ------------------###############
    
 def log(module,msg):
   xbmc.output("### [%s-%s] - %s" % (__scriptname__,module,msg,),level=xbmc.LOGDEBUG ) 
+
+###-------------------------  Hash  -----------------###############
+def hashFile(filename): 
+  try: 
+    longlongformat = '<LL'  # signed long, unsigned long 
+    bytesize = struct.calcsize(longlongformat) 
+    f = open(filename, "rb") 
+        
+    filesize = os.path.getsize(filename)
+    hash = filesize 
+        
+    if filesize < 65536 * 2:
+      return "Error"
+    b = f.read(65536)
+    for x in range(65536/bytesize):
+      buffer = b[x*bytesize:x*bytesize+bytesize]
+      (l2, l1)= struct.unpack(longlongformat, buffer) 
+      l_value = (long(l1) << 32) | long(l2) 
+      hash += l_value 
+      hash = hash & 0xFFFFFFFFFFFFFFFF #to remain as 64bit number
+    
+    f.seek(max(0,filesize-65536),0)
+    b = f.read(65536)
+    for x in range(65536/bytesize):
+      buffer = b[x*bytesize:x*bytesize+bytesize]
+      (l2, l1) = struct.unpack(longlongformat, buffer)
+      l_value = (long(l1) << 32) | long(l2)
+      hash += l_value
+      hash = hash & 0xFFFFFFFFFFFFFFFF
+    
+    f.close() 
+    returnedhash =  "%016x" % hash 
+    return returnedhash
+  
+  except(IOError): 
+    return "IOError"
+
 
 ###-------------------------- match sub to file  -------------################        
 
@@ -61,6 +165,11 @@ def regex_tvshow(compare, file, sub = ""):
     return "","",""    
 
 
+
+def languageTranslate(lang, lang_from, lang_to):
+  for x in LANGUAGES:
+    if lang in x[lang_from] :
+      return x[lang_to]
 
 
 def toOpenSubtitles_two( id ):
