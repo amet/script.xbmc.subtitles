49569c006f990a7fada26493f2b96abed2201eb6
amet <amet.nospam@gmail.com>
2011-05-16 00:55:52 +0400

    A few changes to improve search on asia-team. thx Fran CM

--- script.xbmc.subtitles/resources/lib/services/AsiaTeam/service.py
+++ script.xbmc.subtitles/resources/lib/services/AsiaTeam/service.py
@@ -8,7 +8,7 @@ from utilities import log
 _ = sys.modules[ "__main__" ].__language__
 
 
-main_url = "http://subs.asia-team.net/search.php?term=%s&id=&mtype=0&form=Buscar"
+main_url = "http://subs.asia-team.net/search.php?term=%s&mtype=0&id=0&sort=file_name&order=asc&limit=900" #url changed
 debug_pretext = "asiateam"
 DEBUG = True
 
@@ -23,35 +23,17 @@ subtitle_pattern = "<a href=\"file.php\?id=(.*?)\">(.*?)</a>"
 #====================================================================================================================
 
 
-def getallsubs(searchstring, languageshort, languagelong, file_original_path, subtitles_list, tvshow, season, episode):
-	if languageshort == "es":
-		if len(tvshow) > 0:
-			url = main_url + "?sourceid=xbmc-search&mode=results&search_keywords=" + urllib.quote_plus(searchstring)
-		else:
-			searchstring = re.sub('\([0-9]{4}\)','',searchstring)
-			url = main_url + "?sourceid=xbmc-search&mode=results&search_keywords=" + urllib.quote_plus(searchstring)
-
-	url= url.replace("%s",searchstring)
+def getallsubs(searchstring, languageshort, languagelong, file_original_path, subtitles_list, tvshow, season, episode):		
+		
+	url= main_url.replace("%s",searchstring)
+	url = url.replace(' ','%20') #replace spaces
 	content= geturl(url)
 	log(__name__ ,"%s Getting url: %s" % (debug_pretext, content))
 	for matches in re.finditer(subtitle_pattern, content, re.IGNORECASE | re.DOTALL | re.MULTILINE | re.UNICODE):
 		id = matches.group(1)
 		filename=matches.group(2)
 		server = "http://www.asia-team.net"
-
-		if len(tvshow) > 0:
-
-			if int(episode) < 10:
-				episode2 = "0"+str(episode)
-			else:
-				episode2 = episode
-
-			episodeSerie = season+episode2
-			if filename.find(episodeSerie) >= 0:
-				filename = filename
-				subtitles_list.append({'rating': '0', 'no_files': 1, 'filename': filename, 'server': server, 'sync': False, 'id' : id, 'language_flag': 'flags/' + languageshort + '.gif', 'language_name': languagelong})
-		else:
-			subtitles_list.append({'rating': '0', 'no_files': 1, 'filename': filename, 'server': server, 'sync': False, 'id' : id, 'language_flag': 'flags/' + languageshort + '.gif', 'language_name': languagelong})
+		subtitles_list.append({'rating': '0', 'no_files': 1, 'filename': filename, 'server': server, 'sync': False, 'id' : id, 'language_flag': 'flags/' + languageshort + '.gif', 'language_name': languagelong})
 
 def geturl(url):
     class MyOpener(urllib.FancyURLopener):
@@ -71,11 +53,6 @@ def search_subtitles( file_original_path, title, tvshow, year, season, episode, 
     subtitles_list = []
     msg = ""
     searchstring = title
-    if len(tvshow) == 0:
-        searchstring = title
-    if len(tvshow) > 0:
-        searchstring = "%s S%#02dE%#02d" % (tvshow, int(season), int(episode))
-    log( __name__ ,"%s Search string = %s" % (debug_pretext, searchstring))
 
     spanish = 0
     if string.lower(lang1) == "spanish": spanish = 1
@@ -85,7 +62,7 @@ def search_subtitles( file_original_path, title, tvshow, year, season, episode, 
     getallsubs(searchstring, "es", "Spanish", file_original_path, subtitles_list, tvshow, season, episode)
 
     if spanish == 0:
-        msg = "Won't work, asia-team is only for Spanish subtitles!"
+        msg = "Won't work, Asia-Team is only for Spanish subtitles!"
 
     return subtitles_list, "", msg #standard output
 
@@ -95,14 +72,14 @@ def download_subtitles (subtitles_list, pos, zip_subs, tmp_sub_dir, sub_folder, 
     language = subtitles_list[pos][ "language_name" ]
     filename = subtitles_list[pos][ "filename" ]
     url = "http://subs.asia-team.net/download.php?id=" + id
-
+	
     try:
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         opener = urllib2.build_opener(SmartRedirectHandler())
         content = opener.open(req)
     except ImportError, inst:
-        status,location = inst
+        status,location = inst	
         response= urllib.urlopen(location)
         content=response.read()
     if content is not None:
@@ -130,7 +107,7 @@ def download_subtitles (subtitles_list, pos, zip_subs, tmp_sub_dir, sub_folder, 
         if packed:
             files = os.listdir(tmp_sub_dir)
             init_filecount = len(files)
-            log( __name__ ,"%s asia-team: número de init_filecount %s" % (debug_pretext, init_filecount))
+            log( __name__ ,"%s asia-team: nÃºmero de init_filecount %s" % (debug_pretext, init_filecount))
             filecount = init_filecount
             max_mtime = 0
             # determine the newest file from tmp_sub_dir
@@ -157,33 +134,33 @@ def download_subtitles (subtitles_list, pos, zip_subs, tmp_sub_dir, sub_folder, 
             if waittime == 20:
                 log( __name__ ,"%s Failed to unpack subtitles in '%s'" % (debug_pretext, tmp_sub_dir))
             else:
-                log( __name__ ,"%s Unpacked files in '%s'" % (debug_pretext, tmp_sub_dir))
+                log( __name__ ,"%s Unpacked files in '%s'" % (debug_pretext, tmp_sub_dir))			
                 try:
                     file = choice_one(files) #open new dialog to select file
                     subs_file = os.path.join(tmp_sub_dir,file)
                     return False, language, subs_file #standard output
                 except:
-                    return False, language, "" #standard output
+                    return False, language, "" #standard output				
 
-
+		
 def choice_one(files):
     options = []
     sub_list = []
     Number = 0
-
+    
     for file in files:
-        if (string.split(file, '.')[-1] in ['srt','sub','txt','idx','ssa']):
+        if (string.split(file, '.')[-1] in ['srt','sub','txt','idx','ssa']):		
             Number = Number + 1
             options.append("%02d) %s" % (Number , file))
             sub_list.append(file)
     choice = xbmcgui.Dialog()
-    selection = choice.select("Nº) SUBTITULO", options)
+    selection = choice.select("NÂº) SUBTITULO", options)
     log( __name__ ,"selection=%d" % (selection))
     if selection!= -1:
         return sub_list[selection]
 
 
-class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
+class SmartRedirectHandler(urllib2.HTTPRedirectHandler):	
 	def http_error_302(self, req, fp, code, msg, headers):
 			# Some servers (incorrectly) return multiple Location headers
 			# (so probably same goes for URI).  Use first header.
