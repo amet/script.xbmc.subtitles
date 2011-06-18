74e295298cbd8baa2b024b0ff0d21e6baf1889ce
amet <amet.nospam@gmail.com>
2011-06-18 13:41:54 +0400

    do not try to create hash for rar:// files

--- script.xbmc.subtitles/resources/lib/gui.py
+++ script.xbmc.subtitles/resources/lib/gui.py
@@ -72,6 +72,7 @@ class GUI( xbmcgui.WindowXMLDialog ):
 
     elif (movieFullPath.find("rar://") > -1 ):
       self.rar = True
+      self.temp = True
       movieFullPath = movieFullPath.replace("rar://","")
       if path:
         self.sub_folder = os.path.dirname(os.path.dirname( movieFullPath ))
