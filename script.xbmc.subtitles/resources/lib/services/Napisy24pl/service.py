3d6503d2fe9aa3e21f8897b9f1dd28df66a9263e
mrto <mrto@o2.pl>
2012-09-04 14:47:35 +0200

    Removed napisy.me service (site is down). Added logging to napisy24.pl thx gaco.

--- script.xbmc.subtitles/resources/lib/services/Napisy24pl/service.py
+++ script.xbmc.subtitles/resources/lib/services/Napisy24pl/service.py
@@ -3,11 +3,14 @@
 # Frankenstein Monster v 2.0
 # Feel free to improve, change anything.
 # Credits to amet, Guilherme Jardim, and many more.
+# Big thanks to gaco for adding logging to site.
 # mrto
 
 import urllib2, re, string, xbmc, sys, os
 from utilities import log, languageTranslate
 from BeautifulSoup import BeautifulSoup
+from cookielib import CookieJar
+from urllib import urlencode
 
 _ = sys.modules[ "__main__" ].__language__
 __addon__ = sys.modules[ "__main__" ].__addon__
@@ -22,7 +25,7 @@ elif __addon__.getSetting( "Napisy24_type" ) == "3":
     subtitle_type = "mpl2"
 
 main_url = "http://napisy24.pl/search.php?str="
-base_download_url = "http://napisy.me/download/"
+base_download_url = "http://napisy24.pl/download/"
 down_url = "%s%s/" % (base_download_url, subtitle_type)
 
 def getallsubs(content, title, subtitles_list, file_original_path, stack, lang1, lang2, lang3):
@@ -159,9 +162,12 @@ def search_subtitles( file_original_path, title, tvshow, year, season, episode, 
     return subtitles_list, "", "" #standard output
 
 def download_subtitles (subtitles_list, pos, zip_subs, tmp_sub_dir, sub_folder, session_id): #standard input
-    import urllib
-    f = urllib.urlopen(subtitles_list[pos][ "link" ])
-    language = subtitles_list[pos][ "language_name" ]
+    cj = CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders = [("Referer", "http://napisy24.pl"),  ('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6')]
+    data = urlencode([('form_logowanieMail', __addon__.getSetting( "n24user" )), ('form_logowanieHaslo', __addon__.getSetting( "n24pass" )), ('postAction', 'sendLogowanie')])
+    opener.open("http://napisy24.pl/logowanie/", data)
+    f = opener.open(subtitles_list[pos][ "link" ])
 
     local_tmp_file = os.path.join(tmp_sub_dir, "zipsubs.zip")
     log( __name__ ,"Saving subtitles to '%s'" % (local_tmp_file))
@@ -169,6 +175,7 @@ def download_subtitles (subtitles_list, pos, zip_subs, tmp_sub_dir, sub_folder, 
     local_file = open(zip_subs, "w" + "b")
     local_file.write(f.read())
     local_file.close()
+    opener.open("http://napisy24.pl/index.php?sendAction=Wyloguj")
 
+    language = subtitles_list[pos][ "language_name" ]
     return True,language, "" #standard output
-
